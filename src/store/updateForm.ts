3960020b79c9e7ec99a35136e65f343a70a2bf23
Santo <santokhan1999@gmail.com>
2023-09-11 12:42:00 +0600

    update

--- src/store/updateForm.ts
+++ src/store/updateForm.ts
@@ -1,28 +1,30 @@
-// stores/counter.js
-import { defineStore } from 'pinia'
-import { type VideoDataWith_Id } from '../components/firebase/admin';
+// // stores/counter.js
+// import { defineStore } from 'pinia'
+// import { type VideoDataWith_Id } from '../components/firebase/admin';
 
-export const useUpdateFormStore = defineStore('updateVdoData', {
-    state: () => {
-        return {
-            vdoData: {
-                _id: "",
-                category: "",
-                lesson: -1,
-                description: "",
-                title: "",
-                vdoChiperId: "",
-                courseName: "",
-                vdoStorage: ""
-            }
-        }
-    },
-    actions: {
-        getVdoData() {
-            return this.vdoData;
-        },
-        handleVdoData(data: VideoDataWith_Id) {
-            this.vdoData = data;
-        }
-    },
-})
+// export const useUpdateFormStore = defineStore('updateVdoData', {
+//     state: () => {
+//         return {
+//             vdoData: {
+//                 _id: "",
+//                 category: "",
+//                 lesson: -1,
+//                 description: "",
+//                 title: "",
+//                 vdoChiperId: "",
+//                 courseName: "",
+//                 vdoStorage: "",
+//                 price:0,
+//                 discountPrice:0
+//             }
+//         }
+//     },
+//     actions: {
+//         getVdoData() {
+//             return this.vdoData;
+//         },
+//         handleVdoData(data: VideoDataWith_Id) {
+//             this.vdoData = data;
+//         }
+//     },
+// })
