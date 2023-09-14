0779c68464735bd315b042594e7e92b7e64a9b11
Santo <santokhan1999@gmail.com>
2023-09-14 13:23:13 +0600

    update

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import cors from 'cors'
-import fetch from 'node-fetch'
 
 dotenv.config()
 const port = process.env.PORT || 7002
@@ -13,21 +12,21 @@ app.use(cors())
 app.get("/", async (req, res) => {
     const vdoId = "48860640a4d64b4d809158abd872e5ef";
     // const vdoId = req.query.vdoChiperId
-
-    await fetch(`https://dev.vdocipher.com/api/videos/${vdoId}/otp`, {
-        method: "POST",
-        headers: {
-            Authorization: "Apisecret e2v0ojeaus1EMJWsLru61ztYD5Hj7k9fJKkACeg1XbxbarZ3KwXP4HvnlY6VTpGd",
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-            ttl: 300
-        }),
-    }).then(res => res.json()).then(data => {
-        res.json({ status: data })
-    }).catch(err => {
-        res.send(err)
-    })
+    res.json(vdoId)
+    // await fetch(`https://dev.vdocipher.com/api/videos/${vdoId}/otp`, {
+    //     method: "POST",
+    //     headers: {
+    //         Authorization: "Apisecret e2v0ojeaus1EMJWsLru61ztYD5Hj7k9fJKkACeg1XbxbarZ3KwXP4HvnlY6VTpGd",
+    //         "Content-Type": "application/json",
+    //     },
+    //     body: JSON.stringify({
+    //         ttl: 300
+    //     }),
+    // }).then(res => res.json()).then(data => {
+    //     res.json({ status: data })
+    // }).catch(err => {
+    //     res.send(err)
+    // })
 })
 
 app.listen(port, () => {
