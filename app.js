c1cd68b1ba4dee2ccb33e6e60f7e6b2012e5ed5a
Santo <santokhan1999@gmail.com>
2023-09-14 12:41:41 +0600

    update

--- app.js
+++ app.js
@@ -1,45 +1,45 @@
-// import express from 'express';
-// import dotenv from 'dotenv';
-// import cors from 'cors';
-// import fetch from 'node-fetch'
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import fetch from 'node-fetch'
 
-// dotenv.config()
-// const port = process.env.PORT || 7002
+dotenv.config()
+const port = process.env.PORT || 7002
 
-// const app = express()
+const app = express()
 
-// app.use(cors())
+app.use(cors())
 
-// app.get("/", async (req, res) => {
-//     const vdoId = "48860640a4d64b4d809158abd872e5ef";
-//     // const vdoId = req.query.vdoChiperId
+app.get("/", async (req, res) => {
+    const vdoId = "48860640a4d64b4d809158abd872e5ef";
+    // const vdoId = req.query.vdoChiperId
 
-//     await fetch(`https://dev.vdocipher.com/api/videos/${vdoId}/otp`, {
-//         method: "POST",
-//         headers: {
-//             Authorization: "Apisecret e2v0ojeaus1EMJWsLru61ztYD5Hj7k9fJKkACeg1XbxbarZ3KwXP4HvnlY6VTpGd",
-//             "Content-Type": "application/json",
-//         },
-//         body: JSON.stringify({
-//             ttl: 300
-//         }),
-//     }).then(res => res.json()).then(data => {
-//         res.json({ status: data })
-//     }).catch(err => {
-//         res.send(err)
-//     })
-// })
+    await fetch(`https://dev.vdocipher.com/api/videos/${vdoId}/otp`, {
+        method: "POST",
+        headers: {
+            Authorization: "Apisecret e2v0ojeaus1EMJWsLru61ztYD5Hj7k9fJKkACeg1XbxbarZ3KwXP4HvnlY6VTpGd",
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            ttl: 300
+        }),
+    }).then(res => res.json()).then(data => {
+        res.json({ status: data })
+    }).catch(err => {
+        res.send(err)
+    })
+})
 
-// app.listen(port, () => {
-//     console.log(`Server is running at http://localhost:${port}`);
-// })
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+})
 
-var http = require('http');
-var server = http.createServer(function(req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    var message = 'It works! Santo.\n',
-        version = 'NodeJS ' + process.versions.node + '\n',
-        response = [message, version].join('\n');
-    res.end(response);
-});
-server.listen();
+// var http = require('http');
+// var server = http.createServer(function(req, res) {
+//     res.writeHead(200, {'Content-Type': 'text/plain'});
+//     var message = 'It works! Santo.\n',
+//         version = 'NodeJS ' + process.versions.node + '\n',
+//         response = [message, version].join('\n');
+//     res.end(response);
+// });
+// server.listen();
