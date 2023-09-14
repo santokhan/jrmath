f23356c69bdf5009d7bce4e1931828ab2867eeda
Santo <santokhan1999@gmail.com>
2023-09-14 13:42:34 +0600

    update

--- app.js
+++ app.js
@@ -1,15 +1,36 @@
 const express = require('express')
 const dotenv = require('dotenv')
 const cors = require('cors')
+const axios = require('axios')
 
 dotenv.config()
-const port = process.env.PORT || 7002
+const port = process.env.PORT || 7005
 
 const app = express()
 
 app.use(cors())
 
 app.get("/", async (req, res) => {
+    const vdoId = "48860640a4d64b4d809158abd872e5ef";
+    // const vdoId = req.query.vdoChiperId
+    res.json(vdoId)
+    // await fetch(`https://dev.vdocipher.com/api/videos/${vdoId}/otp`, {
+    //     method: "POST",
+    //     headers: {
+    //         Authorization: "Apisecret e2v0ojeaus1EMJWsLru61ztYD5Hj7k9fJKkACeg1XbxbarZ3KwXP4HvnlY6VTpGd",
+    //         "Content-Type": "application/json",
+    //     },
+    //     body: JSON.stringify({
+    //         ttl: 300
+    //     }),
+    // }).then(res => res.json()).then(data => {
+    //     res.json({ status: data })
+    // }).catch(err => {
+    //     res.send(err)
+    // })
+})
+
+app.get("/api", async (req, res) => {
     const vdoId = "48860640a4d64b4d809158abd872e5ef";
     // const vdoId = req.query.vdoChiperId
     res.json(vdoId)
