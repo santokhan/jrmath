37281a0340d4d6c4be66c23aee9e9a8607a7500d
Santo <santokhan1999@gmail.com>
2023-09-14 13:07:49 +0600

    update

--- app.mjs
+++ app.mjs
@@ -1,7 +1,7 @@
-const express = require('express')
-const dotenv = require('dotenv')
-const cors = require('cors')
-const fetch = require('node-fetch')
+import express from 'express'
+import dotenv from 'dotenv'
+import cors from 'cors'
+import fetch from 'node-fetch'
 
 dotenv.config()
 const port = process.env.PORT || 7002
