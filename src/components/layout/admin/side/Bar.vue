f0f27a31b1ceb885f79e5773567bced3c5201656
Santo <santokhan1999@gmail.com>
2023-10-03 09:54:55 +0600

    update

--- src/components/layout/admin/side/Bar.vue
+++ src/components/layout/admin/side/Bar.vue
@@ -1,10 +1,6 @@
 <template>
     <aside class="w-[15rem] py-8 border-r">
         <nav class="flex flex-col">
-            <!-- <button type="button" @click="props.goToVideoData" :class="[
-                activeNav === NAVS[1] ? ' text-orange-500' : 'hover:bg-orange-100 focus:bg-orange-500 focus:text-white',
-                'text-start font-medium px-4 py-2.5'
-            ]">Video Data</button> -->
             <button type="button" @click="props.goToUserInfo" :class="[
                 activeNav === NAVS[0] ? ' text-orange-500 hover:bg-orange-100' : 'hover:bg-orange-100 focus:bg-orange-500 focus:text-white',
                 'text-start font-medium px-4 py-2.5'
@@ -16,7 +12,6 @@
 <script setup lang="ts">
 const props = defineProps<{
     goToUserInfo: () => void,
-    goToVideoData: () => void,
     activeNav: string,
     NAVS: string[]
 }>()
--- src/views/AdminView.vue
+++ src/views/AdminView.vue
@@ -1,9 +1,8 @@
 <template>
     <main v-if="isAdmin" class="grid grid-cols-1 lg:grid-cols-[15rem_auto]">
-        <Sidebar :goToUserInfo="goToUserInfo" :goToVideoData="goToVideoData" :activeNav="activeNav" :NAVS="NAVS" />
+        <Sidebar :goToUserInfo="goToUserInfo" :activeNav="activeNav" :NAVS="NAVS" />
         <div class="">
             <UserAccess v-if="activeNav === NAVS[0]" />
-            <!-- <VideoData v-if="activeNav === NAVS[1]" /> -->
         </div>
     </main>
     <div v-else class="text-center text-2xl text-orange-500 my-5">You don't have access to this page</div>
@@ -12,18 +11,14 @@
 <script setup lang="ts">
 import { ref, onBeforeMount } from 'vue'
 import UserAccess from '../components/layout/admin/dashboard/UserAccess.vue';
-import VideoData from '../components/layout/admin/dashboard/VideoData.vue';
 import Sidebar from '../components/layout/admin/side/Bar.vue';
 import admin from '../components/firebase/admin';
 
-const NAVS = ['user-access', 'video-data']
-const activeNav = ref(NAVS[1])
+const NAVS = ['user-access']
+const activeNav = ref(NAVS[0])
 
 function goToUserInfo() {
     activeNav.value = NAVS[0]
-}
-function goToVideoData() {
-    activeNav.value = NAVS[1]
 }
 
 const isAdmin = ref(false)
