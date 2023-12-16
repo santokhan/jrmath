0d1f11fab743449f4d065c4c9d48495b82eb6e59
Santo <santokhan1999@gmail.com>
2023-12-16 15:02:32 +0600

    update course card

--- src/components/layout/course/element/CCard.vue
+++ src/components/layout/course/element/CCard.vue
@@ -1,44 +1,42 @@
 <template>
     <div class="w-full max-w-[22rem] space-y-4 bg-white rounded-xl overflow-hidden relative" title="card">
         <div class="">
-            <div class="">
-                <!-- Image actual size 352x240 -->
-                <img v-if="props.data.thubmnails.asset._ref" :src="urlFor(props.data.thubmnails.asset._ref).url()"
-                    alt="thumbnail" class="h-60 w-full object-cover bg-white" title="Image size 352x240 px">
-                <img v-else src="/images/courses/1.png" alt="thumbnail" class="h-60 w-full object-cover bg-white">
-            </div>
-            <div class="relative px-4 py-4 space-y-3 mt-4">
-                <div class="absolute right-0 -top-12 left-0 px-4 h-16 overflow-hidden flex justify-between">
-                    <div v-if="props.data.price"
-                        class="bg-orange-500 text-white h-16 w-16 rounded-full font-bold text-xl border-2 border-white flex flex-col justify-center items-center text-[18px] leading-none text-center relative">
-                        <div v-if="props.data.discount"
-                            class="w-[calc(100%_-_12px)] h-[3px] absolute left-1/2 -translate-x-1/2 top-[38px] bg-red-500">
-                        </div>
-                        <Taka class="w-4 h-4" />{{ props.data.price || 0 }}
-                    </div>
+            <!-- Image actual size 352x240 -->
+            <img v-if="props.data.thubmnails" :src="urlFor(props.data.thubmnails).url()" alt="thumbnail"
+                class="h-60 w-full object-cover bg-white" title="Image size 352x240 px">
+            <img v-else src="/images/courses/1.png" alt="thumbnail" class="h-60 w-full object-cover bg-white">
+        </div>
+        <div class="relative px-4 py-4 space-y-3 mt-4">
+            <div class="absolute right-0 -top-12 left-0 px-4 h-16 overflow-hidden flex justify-between">
+                <div v-if="props.data.price"
+                    class="bg-orange-500 text-white h-16 w-16 rounded-full font-bold text-xl border-2 border-white flex flex-col justify-center items-center text-[18px] leading-none text-center relative">
                     <div v-if="props.data.discount"
-                        class="bg-orange-500 text-white h-16 w-16 rounded-full font-bold text-xl border-2 border-white flex flex-col justify-center items-center text-[18px] leading-none text-center">
-                        <Taka class="w-4 h-4" />{{ props.data.discount || 0 }}
+                        class="w-[calc(100%_-_12px)] h-[3px] absolute left-1/2 -translate-x-1/2 top-[38px] bg-red-500">
+                    </div>
+                    <Taka class="w-4 h-4" />{{ props.data.price || 0 }}
+                </div>
+                <div v-if="props.data.discount"
+                    class="bg-orange-500 text-white h-16 w-16 rounded-full font-bold text-xl border-2 border-white flex flex-col justify-center items-center text-[18px] leading-none text-center">
+                    <Taka class="w-4 h-4" />{{ props.data.discount || 0 }}
+                </div>
+            </div>
+            <div class="flex items-center gap-2">
+                <div class="bg-orange-500 text-white px-1.5 rounded font-medium text-sm">
+                    <div class="text-white flex items-center gap-1">
+                        <div class="w-2 h-2 bg-white rounded-full"></div>{{ props.data.tag || 'All' }}
                     </div>
                 </div>
-                <div class="flex items-center gap-2">
-                    <div class="bg-orange-500 text-white px-1.5 rounded font-medium text-sm">
-                        <div class="text-white flex items-center gap-1">
-                            <div class="w-2 h-2 bg-white rounded-full"></div>{{ props.data.tag || 'All' }}
-                        </div>
-                    </div>
-                    <!-- <div class="text-gray-700">{{ reviews(props.data.reviews) }} reviews</div> -->
+                <!-- <div class="text-gray-700">{{ reviews(props.data.reviews) }} reviews</div> -->
+            </div>
+            <CourseTitle :title="props.data.title" />
+            <div class="flex justify-between items-center py-2">
+                <div class="flex items-center gap-1">
+                    <Video /> {{ videoData.length || 0 }}x Lessons
                 </div>
-                <CourseTitle :title="props.data.title" />
-                <div class="flex justify-between items-center py-2">
-                    <div class="flex items-center gap-1">
-                        <Video /> {{ videoData.length || 0 }}x Lessons
-                    </div>
-                    <div>
-                        <RouterLink
-                            class="text-white bg-orange-500 font-medium rounded-lg px-4 py-2 hover:bg-orange-600 focus:ring-4 focus:ring-orange-300 focus:outline-none"
-                            :to="to(props.data)">Get started</RouterLink>
-                    </div>
+                <div>
+                    <RouterLink
+                        class="text-white bg-orange-500 font-medium rounded-lg px-4 py-2 hover:bg-orange-600 focus:ring-4 focus:ring-orange-300 focus:outline-none"
+                        :to="to(props.data)">Get started</RouterLink>
                 </div>
             </div>
         </div>
@@ -51,20 +49,9 @@ import Taka from '../../../icons/Taka.vue';
 import Video from '../../../icons/Video.vue';
 import CourseTitle from './CourseTitle.vue';
 
-import { client } from '../../../../sanity/sanityClient'
-import imageUrlBuilder from '@sanity/image-url'
+import { urlFor } from '../../../../sanity/sanityClient'
 import sanityAPI from '../../../../api/sanity';
 import { sort_videos } from '../../../courses/playlist/playlist-helper';
-
-// Get a pre-configured url-builder from your sanity client
-const builder = imageUrlBuilder(client)
-
-// Then we like to make a simple function like this that gives the
-// builder an image and returns the builder for you to specify additional
-// parameters:
-function urlFor(source: any) {
-    return builder.image(source)
-}
 
 interface Card {
     _id: number | string
