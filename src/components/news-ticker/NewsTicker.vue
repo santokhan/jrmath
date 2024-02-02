445824d837a18a86453ec464a5a9e0068f6958ce
Santo <santokhan1999@gmail.com>
2024-02-02 12:38:02 +0600

    update news ticker

--- src/components/news-ticker/NewsTicker.vue
+++ src/components/news-ticker/NewsTicker.vue
@@ -1,23 +1,19 @@
 <script setup lang="ts">
 import { ref, onMounted } from 'vue';
 import { client } from '../../sanity/sanityClient';
-const newsList = ref<string[]>([]);
+
+const title = ref<string[]>([]);
 
 onMounted(async () => {
-    await client.fetch(``).then(res => res.json()).then(data => {
-        newsList.value = data.data();
-    });
+    const data = await client.fetch(`*[_type in path("news-ticker")][0]`);
+    title.value = data.title;
 });
 </script>
 
 <template>
-    <div class="max-w-screen-xl mx-auto overflow-hidden bg-orange-50">
+    <div v-if="title" class="max-w-screen-xl mx-auto overflow-hidden bg-orange-50">
         <div class="news-ticker w-full">
-            <h3 class="news-item text-xl">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci harum magnam commodi ab odit rem porro?
-                Deleniti nemo est labore ipsum vel! Minus eum nostrum sunt saepe, odit debitis cum.
-            </h3>
-            <h3 class="news-item" v-for="(item, index) in newsList" :key="index">{{ item }}</h3>
+            <h3 class="news-item text-xl">{{ title }}</h3>
         </div>
     </div>
 </template>
